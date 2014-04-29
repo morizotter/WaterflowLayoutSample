7e65a4e1ed9df24470448a3c9628a54e3a03f18b
Naoki Morita <namorit@gmail.com>
2014-04-29 14:43:59 +0900

    Initial Commit

---
+++ CollectionViewWaterfallLayoutSample/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  CollectionViewWaterfallLayoutSample
+//
+//  Created by MORITA NAOKI on 2014/04/29.
+//  Copyright (c) 2014年 molabo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ CollectionViewWaterfallLayoutSampleTests/CollectionViewWaterfallLayoutSampleTests.m
@@ -0,0 +1,34 @@
+//
+//  CollectionViewWaterfallLayoutSampleTests.m
+//  CollectionViewWaterfallLayoutSampleTests
+//
+//  Created by MORITA NAOKI on 2014/04/29.
+//  Copyright (c) 2014年 molabo. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CollectionViewWaterfallLayoutSampleTests : XCTestCase
+
+@end
+
+@implementation CollectionViewWaterfallLayoutSampleTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
